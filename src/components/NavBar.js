2f82887f8f91c0fff12b8ebf8d38f4dac9315665
Yaen <acostaDtomas.24@gmail.com>
2024-06-03 23:29:34 -0600

    new configuration firebsae, google services and google sign in

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,35 @@
-import React, { useContext, useRef, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import Icons from './Icons';
-import {View, TouchableOpacity, StyleSheet, Text} from 'react-native';
-import {getAuth} from 'firebase/auth';
+import { View, TouchableOpacity, StyleSheet, Text } from 'react-native';
+import { getAuth } from 'firebase/auth';
 import { MenuContext } from '../hooks/MenuContext';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
 const auth = getAuth();
 
 
-export const NavBar = ({name}) => {
-  
-  const { showMenu } = useContext(MenuContext);
+export const NavBar = ({ name }) => {
 
-    const handleLogout = async () => {
-        try {
-          await auth.signOut();
-        } catch (err) {
-          console.log('Error al cerrar sesion: ', err.message);
-        }
-      };
-     
+  const { showMenu } = useContext(MenuContext);
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId: '58442434849-5ejnse52ethv4v25u7nm0la5dri7h8k1.apps.googleusercontent.com',
+    });
+  }, []);
+  const handleLogout = async () => {
+    try {
+      await auth.signOut();
+
+      GoogleSignin.revokeAccess();
+
+    } catch (err) {
+      console.log('Error al cerrar sesion: ', err.message);
+    }
+  };
+
   return (
     <View style={styles.navbar}>
-      <TouchableOpacity  onPress={showMenu}>
+      <TouchableOpacity onPress={showMenu}>
         <Icons name={'bars'} sizes={25} />
       </TouchableOpacity>
       <Text style={styles.title}>{name}</Text>
@@ -50,10 +58,10 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginRight: 15,
   },
-  menuContainer:{
-    backgroundColor:'white',
-    width:'100%',
-    height:'100%'
+  menuContainer: {
+    backgroundColor: 'white',
+    width: '100%',
+    height: '100%'
   }
-  
+
 });
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Button,
   Image,
@@ -13,94 +13,68 @@ import {
   Alert,
 } from 'react-native';
 import pollo from '../../Assets/fnbg.png';
-import {GoogleAuthProvider, signInWithEmailAndPassword} from 'firebase/auth';
-// import {auth} from '../../config';
-import {BannerAd, BannerAdSize} from '@react-native-admob/admob';
-import {
-  GoogleOneTapSignIn,
-  statusCodes,
-  isErrorWithCode,
-  GoogleSignin,
-} from '@react-native-google-signin/google-signin';
-import auth from '@react-native-firebase/auth';
-export const Login = ({navigation}) => {
-  //Esto es para guardar los datos que el usuario ingrese en el form
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithCredential } from 'firebase/auth';
+import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/google-signin';
+import { BannerAd, BannerAdSize } from '@react-native-admob/admob';
+import { auth } from '../../config';
 
-  //START
+
+export const Login = ({ navigation }) => {
   const [user, setUser] = useState('');
   const [pass, setPass] = useState('');
 
-  const handleUserData = text => {
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId: '58442434849-5ejnse52ethv4v25u7nm0la5dri7h8k1.apps.googleusercontent.com',
+    });
+  }, []);
+
+  const handleUserData = (text) => {
     setUser(text);
   };
-  const handleUserPass = text => {
+
+  const handleUserPass = (text) => {
     setPass(text);
   };
-  //END
-
-  //Funciones para loguearse en base a firebase
 
   const LoginToApp = () => {
-    if (
-      user !== undefined &&
-      user !== '' &&
-      pass !== undefined &&
-      pass !== ''
-    ) {
-      try {
-        //const auth = getAuth();
-
-        signInWithEmailAndPassword(auth, user, pass)
-          .then(() => {
-            Alert.alert('Usuario logueado con éxito');
-          })
-          .catch(err => {
-            Alert.alert(err);
-          });
-      } catch (error) {
-        console.error('Error al iniciar sesión:', error);
-      }
+    if (user && pass) {
+      signInWithEmailAndPassword(auth, user, pass)
+        .then(() => {
+          Alert.alert('Usuario logueado con éxito');
+        })
+        .catch((err) => {
+          Alert.alert(err.message);
+        });
     } else {
       Alert.alert(
         'Error',
-        'El usuario o contraseña no puede estar vacio, Por favor vuelve a intentarlo',
+        'El usuario o contraseña no puede estar vacio, Por favor vuelve a intentarlo'
       );
     }
   };
 
-  //Login with google
-  GoogleSignin.configure({
-    webClientId:
-      '58442434849-3p8jjgqd2v873iddjng13dfrpn3kpoaq.apps.googleusercontent.com',
-  });
-
-  _signIn = async () => {
+  async function onGoogleButtonPress() {
     try {
-      await GoogleSignin.hasPlayServices();
-      const {idToken} = await GoogleSignin.signIn();
-      const googleCredentials = auth.GoogleAuthProvider.credential(idToken)
-
-      return auth().signInWithCredential(googleCredentials);
-
+      await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+      const { idToken } = await GoogleSignin.signIn();
+      const googleCredential = GoogleAuthProvider.credential(idToken);
+      const userCredential = await signInWithCredential(auth, googleCredential);
+      console.log(userCredential);
+      return userCredential;
     } catch (error) {
-      console.log("es el error:"+ error)
-      if (error) {
-        switch (error.code) {
-          case statusCodes.SIGN_IN_CANCELLED:
-            // user cancelled the login flow
-            break;
-          case statusCodes.IN_PROGRESS:
-            // operation (eg. sign in) already in progress
-            break;
-          case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
-            // play services not available or outdated
-            break;
-          default:
-          // some other error happened
-        }
-      } else {
-        // an error that's not related to google sign in occurred
-      }
+      console.error('Error en Sign In: ', error);
+      throw error;
+    }
+  }
+
+  const handleSignIn = async () => {
+    try {
+      await onGoogleButtonPress();
+      const signedInUser = auth.currentUser;
+      console.log('User Signed In: ', signedInUser?.displayName);
+    } catch (error) {
+      console.log('Error en Sign In: ', error);
     }
   };
 
@@ -109,15 +83,12 @@ export const Login = ({navigation}) => {
       source={require('../../Assets/fondo.jpg')}
       style={styles.backgroundImage}
       resizeMode="cover"
-      imageStyle={{opacity: 0.08}}>
+      imageStyle={{ opacity: 0.08 }}
+    >
       <ScrollView contentContainerStyle={styles.scrollViewContent}>
-        <StatusBar
-          translucent
-          backgroundColor={'transparent'}
-          barStyle="dark-content"
-        />
+        <StatusBar translucent backgroundColor={'transparent'} barStyle="dark-content" />
         <View style={styles.container}>
-          <Image style={{width: 400, height: 400}} source={pollo} />
+          <Image style={{ width: 400, height: 400 }} source={pollo} />
           <Text style={styles.textUsers}>Usuario: </Text>
           <TextInput
             style={styles.inputType}
@@ -133,22 +104,12 @@ export const Login = ({navigation}) => {
             placeholder="Ingrese su Contraseña"
             onChangeText={handleUserPass}
           />
-          <TouchableOpacity style={styles.button} onPress={() => LoginToApp()}>
-            <Text style={{color: 'white', textAlign: 'center'}}>
-              Iniciar Sesión
-            </Text>
+          <TouchableOpacity style={styles.button} onPress={LoginToApp}>
+            <Text style={{ color: 'white', textAlign: 'center' }}>Iniciar Sesión</Text>
           </TouchableOpacity>
-          <Button
-            title="Google Sign-In"
-            onPress={() =>
-              _signIn()
-            }
-          />
+          <GoogleSigninButton onPress={handleSignIn} disabled={false} />
         </View>
-        <BannerAd
-          unitId="ca-app-pub-3477493054350988/1457774401"
-          size={BannerAdSize.ADAPTIVE_BANNER}
-        />
+        <BannerAd unitId="ca-app-pub-3477493054350988/1457774401" size={BannerAdSize.ADAPTIVE_BANNER} />
       </ScrollView>
     </ImageBackground>
   );
